8f2f0ab388ef31931f67bdad42314bc5bf7258f6
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2024-06-29 15:52:48 -0400

    Removed TimeToLive property from IRipeConfiguration

--- dotnet/Ripe.Sdk.Core/IRipeConfiguration.cs
+++ dotnet/Ripe.Sdk.Core/IRipeConfiguration.cs
@@ -1,15 +1,12 @@
-﻿namespace Ripe.Sdk.Core
+﻿using System;
+
+namespace Ripe.Sdk.Core
 {
     /// <summary>
     /// Interface to implement on your configuration objects
     /// </summary>
     public interface IRipeConfiguration
     {
-        /// <summary>
-        /// The interval at which the Ripe Service will refresh the cache with changes to your Ripe configuration. Used for configuring 
-        /// when to automatically attempt to refresh configuration data
-        /// </summary>
-        int TimeToLive { get; set; }
         /// <summary>
         /// The version of the Ripe API you have connected to
         /// </summary>
